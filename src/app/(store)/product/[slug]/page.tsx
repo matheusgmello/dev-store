da8c3af47824738b5ca562a3d9d129687073981e
Matheus Gabriel <matheusgmello1@gmail.com>
2024-01-19 19:06:17 -0300

    Feat: Adicionando ao carrinho

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
+import { Metadata } from 'next'
 
 import { api } from '@/data/api'
 import { Product } from '@/data/types/product'
-import { Metadata } from 'next'
+import { AddToCartButton } from '@/components/add-to-cart-button'
 
 interface ProductProps {
   params: {
@@ -26,6 +27,7 @@ export async function generateMetadata({
   params,
 }: ProductProps): Promise<Metadata> {
   const product = await getProduct(params.slug)
+
   return {
     title: product.title,
   }
@@ -39,6 +41,7 @@ export async function generateStaticParams() {
     return { slug: product.slug }
   })
 }
+
 export default async function ProductPage({ params }: ProductProps) {
   const product = await getProduct(params.slug)
 
@@ -71,6 +74,7 @@ export default async function ProductPage({ params }: ProductProps) {
             })}
           </span>
           <span className="text-sm text-zinc-400">
+            Em até 12x s/ juros de{' '}
             {(product.price / 12).toLocaleString('pt-BR', {
               style: 'currency',
               currency: 'BRL',
@@ -109,12 +113,7 @@ export default async function ProductPage({ params }: ProductProps) {
           </div>
         </div>
 
-        <button
-          type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
-        >
-          Adicionar ao carrinho
-        </button>
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   )
